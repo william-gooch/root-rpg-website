ab43e2f0f2e9246cd3df19fd8699b2cba0ea678f
William Gooch <william.gooch.wg@gmail.com>
2021-01-28 13:09:04 +0000

    Add bindings for Background.

--- src/CharacterPage/Background/Background.tsx
+++ src/CharacterPage/Background/Background.tsx
@@ -1,27 +1,45 @@
 import { Grid, TextField } from "@material-ui/core";
 import React from "react"
+import { useCharacterProperty } from "../../CharacterProvider";
 
-interface BackgroundProps {
-  
-}
+const Background: React.FC = () => {
+  const [backgroundWhere, setBackgroundWhere] = useCharacterProperty("backgroundWhere");
+  const [backgroundWhy, setBackgroundWhy] = useCharacterProperty("backgroundWhy");
+  const [backgroundWho, setBackgroundWho] = useCharacterProperty("backgroundWho");
+  const [backgroundFactionServed, setBackgroundFactionServed] = useCharacterProperty("backgroundFactionServed");
+  const [backgroundFactionEnmity, setBackgroundFactionEnmity] = useCharacterProperty("backgroundFactionEnmity");
 
-const Background: React.FC<BackgroundProps> = props => {
   return (
     <Grid item container direction="column" className="background-box">
       <Grid item className="text-field">
-        <TextField fullWidth multiline variant="outlined" label="Where do you call home?" />
+        <TextField fullWidth multiline variant="outlined" label="Where do you call home?"
+          value={backgroundWhere}
+          onChange={evt => setBackgroundWhere(evt.target.value)}
+        />
       </Grid>
       <Grid item className="text-field">
-        <TextField fullWidth multiline variant="outlined" label="Why are you a vagabond?" />
+        <TextField fullWidth multiline variant="outlined" label="Why are you a vagabond?"
+          value={backgroundWhy}
+          onChange={evt => setBackgroundWhy(evt.target.value)}
+        />
       </Grid>
       <Grid item className="text-field">
-        <TextField fullWidth multiline variant="outlined" label="Whom have you left behind?" />
+        <TextField fullWidth multiline variant="outlined" label="Whom have you left behind?"
+          value={backgroundWho}
+          onChange={evt => setBackgroundWho(evt.target.value)}
+        />
       </Grid>
       <Grid item className="text-field">
-        <TextField fullWidth multiline variant="outlined" label="Which faction have you served the most?" />
+        <TextField fullWidth multiline variant="outlined" label="Which faction have you served the most?"
+          value={backgroundFactionServed}
+          onChange={evt => setBackgroundFactionServed(evt.target.value)}
+        />
       </Grid>
       <Grid item className="text-field">
-        <TextField fullWidth multiline variant="outlined" label="With which faction have you earned a special enmity?" />
+        <TextField fullWidth multiline variant="outlined" label="With which faction have you earned a special enmity?"
+          value={backgroundFactionEnmity}
+          onChange={evt => setBackgroundFactionEnmity(evt.target.value)}
+        />
       </Grid>
     </Grid>
   );
